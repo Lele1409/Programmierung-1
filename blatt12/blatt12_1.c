c441eed90919d7ef92bade7d96667e6b8ba96386
Lele1409 <leander.decoene@gmail.com>
2025-02-24 11:47:28 +0100

    Add: woche 12

--- blatt12/blatt12_1.c
+++ blatt12/blatt12_1.c
@@ -3,27 +3,25 @@
 int sum(int n) {
     if (n <= 1) {
         return n;
-    } else {
-        return n + sum(n - 1);
     }
+    return n + sum(n - 1);
 }
 
 int fakultaet(int n) {
     if (n <= 1) {
         return 1;
-    } else {
-        return n * fakultaet(n - 1);
     }
+    return n * fakultaet(n - 1);
 }
 
 int fibonacci(int n) {
     if (n == 0) {
         return 0;
-    } else if (n == 1) {
-        return 1;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
     }
+    if (n == 1) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main() {
--- blatt12/blatt12_2.c
+++ blatt12/blatt12_2.c
@@ -2,13 +2,14 @@
 
 // Rekursive Implementierung der Ackermann-Funktion
 unsigned int ackermann(unsigned int n, unsigned int m) {
+    printf("%d %d\n", n, m);
     if (n == 0) {
         return m + 1;
-    } else if (m == 0) {
-        return ackermann(n - 1, 1);
-    } else {
-        return ackermann(n - 1, ackermann(n, m - 1));
     }
+    if (m == 0) {
+        return ackermann(n - 1, 1);
+    }
+    return ackermann(n - 1, ackermann(n, m - 1));
 }
 
 int main() {
